6d1d1592423294ae0fccdc794cfde15c977f96f7
Mao-70R7 <Mao-70R7@users.noreply.github.com>
2026-07-11 22:42:03 +0800

    Refresh mixed performance report data

--- basic_data/data/public_fund_performance_snapshot.js
+++ basic_data/data/public_fund_performance_snapshot.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5cb58e4e7089d2e522da4b519a2be0939e593f1a1a5ff267e4ae93bfc11afcb
-size 238311670
+oid sha256:7059ab50a1adc88f91f4270a6f7c1760a2df3d943057ddd0c17fbc5803cb66ca
+size 238766393
